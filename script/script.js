9f4dfbab283e4624bd0c21cfb1cfe9584d96d827
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-07-22 14:46:41 -0300

    creat the Java Scrip routine that gets the client order and values and checked if all the tree itens was selected

---
+++ script/script.js
@@ -0,0 +1,34 @@
+const foodMenu = document.querySelector('form.food');
+const drinkMenu = document.querySelector('form.drink');
+const dessertMenu = document.querySelector('form.dessert');
+
+foodMenu.addEventListener('change',updateFood);
+drinkMenu.addEventListener('change',updateDrink);
+dessertMenu.addEventListener('change',updateDessert);
+
+const order = {
+    food: [],
+    drink: [],
+    dessert: [],
+}
+let closedOrder = false;
+
+function updateFood(e){
+    order.food = [e.target.id,e.target.value];
+    if(Object.values(order).filter(i => i.length===2).length === 3){
+        closedOrder = true
+    }
+}
+function updateDrink(e){
+    order.drink = [e.target.id,e.target.value];
+    if(Object.values(order).filter(i => i.length===2).length === 3){
+        closedOrder = true
+    }
+}
+function updateDessert(e){
+    order.dessert = [e.target.id,e.target.value];
+    if(Object.values(order).filter(i => i.length===2).length === 3){
+        closedOrder = true
+    }
+}
+
